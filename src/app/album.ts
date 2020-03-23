7676428383f302a6df268a279098ded38e1608a0
alon121 <nextmedia4u@gmail.com>
2020-03-23 20:18:32 +0200

    upload files

--- src/app/album.ts
+++ src/app/album.ts
@@ -3,9 +3,5 @@ export class Album {
     name:string;
     albums:Song[];
     description:string;
-    constructor(name:string, albums:Song[], description:string){
-        this.name = name;
-        this.albums = albums;
-        this.description = description;
-    }
+
 };
